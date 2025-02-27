06824421120f457c6f6bdab81f3bc07746e62d2c
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-02-27 11:11:19 +0000

    REFACTOR PLAIN CONSTRUCTOR INTO CLASS

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 
 const myLibrary = [];
 
-function Book(title, author, numberOfPages, read) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-}
+// function Book(title, author, numberOfPages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.numberOfPages = numberOfPages;
+//     this.read = read;
+// }
 
-class
+class Book {
+    constructor(title, author, numberOfPages, read){
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.read = read;
+    }
+}
 function addBookToLibrary(title, author, numberOfPages, read) {
     const newBook = new Book(title, author, numberOfPages, read);
     myLibrary.push(newBook);

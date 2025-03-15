9e0491a2f30695ff03bd5c4d1cb491a22e9fc571
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-03-15 09:49:42 +0000

    constraint validation on the form and setCustomValidity to set error message

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const myLibrary = [];
 
 // function Book(title, author, numberOfPages, read) {
@@ -9,16 +8,16 @@ const myLibrary = [];
 // }
 
 class Book {
-    constructor(title, author, numberOfPages, read){
-        this.title = title;
-        this.author = author;
-        this.numberOfPages = numberOfPages;
-        this.read = read;
-    }
+  constructor(title, author, numberOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+  }
 }
 function addBookToLibrary(title, author, numberOfPages, read) {
-    const newBook = new Book(title, author, numberOfPages, read);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, numberOfPages, read);
+  myLibrary.push(newBook);
 }
 
 const container = document.querySelector(".container");
@@ -33,7 +32,7 @@ const form = document.createElement("form");
 form.action = "#";
 form.method = "POST";
 dialog.appendChild(form);
-
+ 
 const label1 = document.createElement("label");
 label1.for = "Title";
 label1.textContent = "Title";
@@ -41,11 +40,10 @@ const input1 = document.createElement("input");
 input1.type = "text";
 input1.name = "Title";
 input1.id = "Title";
+input1.required = true;
 
 form.appendChild(label1);
 form.appendChild(input1);
-
-
 
 //Author
 
@@ -56,6 +54,7 @@ const input2 = document.createElement("input");
 input2.type = "text";
 input2.name = "Author";
 input2.id = "Author";
+input2.required = true;
 
 form.appendChild(label2);
 form.appendChild(input2);
@@ -69,6 +68,7 @@ const input3 = document.createElement("input");
 input3.type = "number";
 input3.name = "Pages";
 input3.id = "Pages";
+input3.required = true;
 
 form.appendChild(label3);
 form.appendChild(input3);
@@ -82,11 +82,10 @@ const input4 = document.createElement("input");
 input4.type = "checkbox";
 input4.name = "Read";
 input4.id = "Read";
+input4.requried = true;
 
 form.appendChild(label4);
 form.appendChild(input4);
-
-
 
 const addButton = document.createElement("button");
 const closeButton = document.createElement("button");
@@ -96,99 +95,100 @@ closeButton.textContent = "Close";
 dialog.appendChild(addButton);
 dialog.appendChild(closeButton);
 
+button.addEventListener("click", () => {
+  // console.log("button clicked!");
 
-button.addEventListener('click', () => {
-    console.log("button clicked!");
+  input1.value = "";
+  input2.value = "";
+  input3.value = "";
+  input4.checked = false;
 
-    input1.value = "";
-    input2.value = "";
-    input3.value = "";
-    input4.checked = false;
-
-    dialog.showModal();
-
+  dialog.showModal();
 });
 
 
-addButton.addEventListener('click', (event) => {
+addButton.addEventListener("click", (event) => {
+
     event.preventDefault();
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book");
-    container.appendChild(bookDiv);
 
-    //title
-    const title = document.createElement("p");
-    title.textContent = input1.value;
-    title.style.cssText = "font-weight: bold; font-size: 20px";
-    bookDiv.appendChild(title);
+    let isValid = true; 
 
-    //author
-    const author = document.createElement("p");
-    author.textContent = `By ${input2.value}`;
-    bookDiv.appendChild(author);
+    const requiredInputs = document.querySelectorAll('input[required]');
 
-    //pages
-    const pages = document.createElement("p");
-    pages.textContent = `${input3.value} pages`;
-    bookDiv.appendChild(pages);
+    requiredInputs.forEach(input => {
+        if (input.value.trim() === '') {
+            isValid = false;
 
-    //read
-    const read = document.createElement("p");
-    //what is this code for ? 
-    let readStatus = "Read";
-    if (input4.checked) {
-        read.textContent = "Read";
-    } else {
-        read.textContent = "Not read";
-        readStatus = "Not read";
-    }
-    // read.textContent = input4.value;
-    bookDiv.appendChild(read);
-
-    let newBook = new Book(input1.value, input2.value, input3.value, readStatus);
-    console.log(newBook);
-    Book.prototype.toggle_read = function () {
-        if (this.read === "Read") {
-            this.read = "Not Read";
-        } else {
-            this.read = "Read";
+            input.setCustomValidity('This field is required');
+            const errorElement = document.createElement("span");
+            errorElement.textContent = input.validationMessage;
+            input.parentNode.appendChild(errorElement);
         }
+    });
+    if(isValid) {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+        container.appendChild(bookDiv);
+
+        //title
+        const title = document.createElement("p");
+        title.textContent = input1.value;
+        title.style.cssText = "font-weight: bold; font-size: 20px";
+        bookDiv.appendChild(title);
+
+        //author
+        const author = document.createElement("p");
+        author.textContent = `By ${input2.value}`;
+        bookDiv.appendChild(author);
+
+        //pages
+        const pages = document.createElement("p");
+        pages.textContent = `${input3.value} pages`;
+        bookDiv.appendChild(pages);
+
+        //read
+        const read = document.createElement("p");
+        //what is this code for ?
+        let readStatus = "Read";
+        if (input4.checked) {
+            read.textContent = "Read";
+        } else {
+            read.textContent = "Not read";
+            readStatus = "Not read";
+        }
+        // read.textContent = input4.value;
+        bookDiv.appendChild(read);
+
+        let newBook = new Book(input1.value, input2.value, input3.value, readStatus);
+        // console.log(newBook);
+        Book.prototype.toggle_read = function () {
+            if (this.read === "Read") {
+            this.read = "Not Read";
+            } else {
+            this.read = "Read";
+            }
+        };
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        bookDiv.appendChild(removeButton);
+        const toggleRead = document.createElement("button");
+        toggleRead.textContent = "Toggle Read";
+        bookDiv.appendChild(toggleRead);
+        removeButton.addEventListener("click", () => {
+            bookDiv.remove();
+        });
+        toggleRead.addEventListener("click", () => {
+            // console.log("toggle read button clicked");
+            newBook.toggle_read();
+            read.textContent = newBook.read;
+        });
+
+        dialog.close();
     }
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    bookDiv.appendChild(removeButton);
-    const toggleRead = document.createElement("button");
-    toggleRead.textContent = "Toggle Read";
-    bookDiv.appendChild(toggleRead);
-    removeButton.addEventListener('click', () => {
-        bookDiv.remove();
-    });
-    toggleRead.addEventListener('click', () => {
-        console.log("toggle read button clicked");
-        newBook.toggle_read();
-        read.textContent = newBook.read;
-    });
-
-    
-    dialog.close();
 });
 
-closeButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log("close button clicked");
-    dialog.close();
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  // console.log("close button clicked");
+  dialog.close();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
